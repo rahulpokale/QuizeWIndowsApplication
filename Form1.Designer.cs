b19715645ce895958438f2a040ad3718cf5962fd
Rahul Pokale <rahulpokales@gmail.com>
2023-04-13 01:20:29 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace QuizeWIndowsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.headerName_lbl = new System.Windows.Forms.Label();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.next_btn = new System.Windows.Forms.Button();
+            this.question_timer = new System.Windows.Forms.Timer(this.components);
+            this.timeTicks_lbl = new System.Windows.Forms.Label();
+            this.instructionText = new System.Windows.Forms.Label();
+            this.result_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // headerName_lbl
+            // 
+            this.headerName_lbl.AutoSize = true;
+            this.headerName_lbl.BackColor = System.Drawing.SystemColors.Control;
+            this.headerName_lbl.Font = new System.Drawing.Font("Sitka Text", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerName_lbl.ForeColor = System.Drawing.Color.SteelBlue;
+            this.headerName_lbl.Location = new System.Drawing.Point(377, 9);
+            this.headerName_lbl.Name = "headerName_lbl";
+            this.headerName_lbl.Size = new System.Drawing.Size(440, 72);
+            this.headerName_lbl.TabIndex = 0;
+            this.headerName_lbl.Text = "Quiz Application";
+            // 
+            // start_btn
+            // 
+            this.start_btn.BackColor = System.Drawing.Color.SeaGreen;
+            this.start_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_btn.ForeColor = System.Drawing.Color.MintCream;
+            this.start_btn.Location = new System.Drawing.Point(474, 351);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(244, 80);
+            this.start_btn.TabIndex = 1;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = false;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // next_btn
+            // 
+            this.next_btn.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.next_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next_btn.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.next_btn.Location = new System.Drawing.Point(519, 437);
+            this.next_btn.Name = "next_btn";
+            this.next_btn.Size = new System.Drawing.Size(159, 39);
+            this.next_btn.TabIndex = 2;
+            this.next_btn.Text = "Next";
+            this.next_btn.UseVisualStyleBackColor = false;
+            this.next_btn.Visible = false;
+            this.next_btn.Click += new System.EventHandler(this.next_btn_Click);
+            // 
+            // question_timer
+            // 
+            this.question_timer.Interval = 1000;
+            this.question_timer.Tick += new System.EventHandler(this.question_timer_Tick);
+            // 
+            // timeTicks_lbl
+            // 
+            this.timeTicks_lbl.AutoSize = true;
+            this.timeTicks_lbl.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeTicks_lbl.Location = new System.Drawing.Point(902, 36);
+            this.timeTicks_lbl.Name = "timeTicks_lbl";
+            this.timeTicks_lbl.Size = new System.Drawing.Size(115, 27);
+            this.timeTicks_lbl.TabIndex = 3;
+            this.timeTicks_lbl.Text = "Time starts";
+            this.timeTicks_lbl.Visible = false;
+            // 
+            // instructionText
+            // 
+            this.instructionText.AutoSize = true;
+            this.instructionText.Font = new System.Drawing.Font("Prestige Elite Std", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionText.Location = new System.Drawing.Point(232, 148);
+            this.instructionText.Name = "instructionText";
+            this.instructionText.Size = new System.Drawing.Size(82, 23);
+            this.instructionText.TabIndex = 4;
+            this.instructionText.Text = "label1";
+            // 
+            // result_lbl
+            // 
+            this.result_lbl.AutoSize = true;
+            this.result_lbl.Font = new System.Drawing.Font("Palatino Linotype", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_lbl.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.result_lbl.Location = new System.Drawing.Point(80, 229);
+            this.result_lbl.Name = "result_lbl";
+            this.result_lbl.Size = new System.Drawing.Size(202, 81);
+            this.result_lbl.TabIndex = 5;
+            this.result_lbl.Text = "label1";
+            this.result_lbl.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 586);
+            this.Controls.Add(this.result_lbl);
+            this.Controls.Add(this.instructionText);
+            this.Controls.Add(this.timeTicks_lbl);
+            this.Controls.Add(this.next_btn);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.headerName_lbl);
+            this.Name = "Form1";
+            this.Text = "Quiz Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerName_lbl;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button next_btn;
+        private System.Windows.Forms.Timer question_timer;
+        private System.Windows.Forms.Label timeTicks_lbl;
+        private System.Windows.Forms.Label instructionText;
+        private System.Windows.Forms.Label result_lbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace QuizeWIndowsApplication
+{
+    public partial class Form1 : Form
+    {
+        private int timerTicks = 0;
+        private int index = 0;
+        private float obtainedMarks = 0;
+        private float totalMarks = 0;
+        private float percentage = 0;
+        FlowLayoutPanel pnl = new FlowLayoutPanel();
+        
+        private class Questions
+        {
+            public string Question { get; set; }
+            public List<string> Options { get; set; }
+            public string RightAnswer { get; set; }
+            public int Marks { get; set; }
+        }
+
+        private List<Questions> questions = new List<Questions>
+        {
+            new Questions
+            {
+                Question = "What is the largest continent in the world?",
+                Options = new List<string> { "North America", "Europe", "Asia", "Australia" },
+                RightAnswer = "Asia",
+                Marks = 10
+            },
+            new Questions
+            {
+                Question = "Which planet is known as the Red Planet?",
+                Options = new List<string> { "Venus", "Mars", "Saturn", "Jupiter" },
+                RightAnswer = "Mars",
+                Marks = 5
+            },
+            new Questions
+            {
+                Question = "Who discovered the theory of relativity?",
+                Options = new List<string> { "Isaac Newton", "Albert Einstein", "Galileo Galilei", "Stephen Hawking" },
+                RightAnswer = "Albert Einstein",
+                Marks = 10
+            },
+            new Questions
+            {
+                Question = "Which river is the longest river in the world?",
+                Options = new List<string> { "Amazon River", "Yangtze River", "Nile River", "Mississippi River" },
+                RightAnswer = "Nile River",
+                Marks = 8
+            },
+            new Questions
+            {
+                Question = "Which is the largest bird in the world?",
+                Options = new List<string> { "Ostrich", "Eagle", "Albatross", "Penguin" },
+                RightAnswer = "Ostrich",
+                Marks = 4
+            }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            List<String> instructionList = new List<String>
+            {
+                "* Each question will have 4 options.",
+                "* You will get 10 seconds for each question.",
+                "* Each question may have different marks.",
+                "* After all questions, result will be displayed at the end."
+            };
+
+            instructionText.Text = String.Join(Environment.NewLine, instructionList);
+            pnl.Dock = DockStyle.None;
+            pnl.Location = new System.Drawing.Point(60, 128);
+            pnl.Height = 300;
+            pnl.Width = 300;
+            pnl.Left = 220;
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            start_btn.Visible = false;
+            next_btn.Visible = true;
+            timeTicks_lbl.Visible = true;
+            instructionText.Visible = false;
+
+            DisplayQuestionAndOptions();
+        }
+
+        private void next_btn_Click(object sender, EventArgs e)
+        {
+            NextQuestion();
+        }
+
+        private void NextQuestion()
+        {
+            RadioButton rbSelected = pnl.Controls
+                                     .OfType<RadioButton>()
+                                     .FirstOrDefault(r => r.Checked);
+            if (rbSelected != null && rbSelected.Text == questions[index - 1].RightAnswer)
+            {
+                obtainedMarks += questions[index - 1].Marks;
+            }
+
+            if (index != questions.Count)
+            {
+                DisplayQuestionAndOptions();
+            }
+            else
+            {
+                pnl.Controls.Clear();
+                next_btn.Visible = false;
+                timeTicks_lbl.Visible = false;
+
+                percentage = (int)Math.Round((double)(obtainedMarks * 100) / totalMarks);
+                result_lbl.Text = "Obtained Marks is : " + obtainedMarks + " / out of "+ totalMarks +(" + percentage + "%)";
+                result_lbl.Visible = true;
+            }
+        }
+
+        private void DisplayQuestionAndOptions()
+        {
+            question_timer.Stop();
+            timerTicks = 0;
+            pnl.Controls.Clear();
+
+            Label lblobj = new Label();
+            lblobj.Text = questions[index].Question + Environment.NewLine;
+            lblobj.Font = new Font(lblobj.Font, lblobj.Font.Style | FontStyle.Bold);
+            lblobj.BackColor = Color.Transparent;
+            lblobj.ForeColor = Color.Black;
+            lblobj.AutoSize = true;
+            pnl.Controls.Add(lblobj);
+
+            RadioButton[] questionOptions = new RadioButton[questions[index].Options.Count];
+            totalMarks += questions[index].Marks;
+            for (int i = 0; i < questions[index].Options.Count; i++)
+            {
+                questionOptions[i] = new RadioButton
+                {
+                    Name = "option_" + i,
+                    Text = questions[index].Options[i],
+                    AutoSize = true,
+                    Font = new System.Drawing.Font("Calibri", 11F, FontStyle.Regular),
+                };
+
+                pnl.Controls.Add(questionOptions[i]);
+            }
+            this.Controls.Add(pnl);
+            index++;
+
+            question_timer.Start();
+        }
+
+        private void question_timer_Tick(object sender, EventArgs e)
+        {
+            timerTicks++;
+            timeTicks_lbl.Text = timerTicks.ToString() + " seconds !";
+
+            if (timerTicks == 10)
+            {
+                timeTicks_lbl.Text = "Times Up !";
+                question_timer.Stop();
+                timerTicks = 0;
+                NextQuestion();
+            }
+        }
+    }
+}
